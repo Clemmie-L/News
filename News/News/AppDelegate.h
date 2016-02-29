5cedf7348f9d62c56475b8fa57752af357962e4d
刘健 <you@example.com>
2016-02-29 11:24:47 +0800

    新建项目

---
+++ News/News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  News
+//
+//  Created by 刘健 on 16/2/29.
+//  Copyright © 2016年 Clemmie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ News/News/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  News
+//
+//  Created by 刘健 on 16/2/29.
+//  Copyright © 2016年 Clemmie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
